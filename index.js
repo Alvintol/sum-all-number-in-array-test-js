6bab4e9577af7b88264d9d27a7576f5a4a222447
Alvin <alvin.tol@hotmail.com>
2022-03-12 22:13:19 +0000

    create function that usies sums all numbers in an array of arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+
+const sumItems = array => {
+  let total = 0;
+
+  array.forEach(num => Array.isArray(num) ? total += sumItems(num) : total += num);
+  return total;
 }
 
 module.exports = sumItems;
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+
+console.log('FINAL ANSWER:', sumItems([[5, 4, [[3], 2]], 1, []]));
+
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
